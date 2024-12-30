5c7f70065978549582039c8cc5eed68b24265d11
anuchit22 <158791968+anuchit22@users.noreply.github.com>
2024-12-30 14:40:42 +0700

    allde
    allde

--- React-Anuchit/newlern/src/components/Hellocomponent.jsx
+++
@@ -1,9 +0,0 @@
-function HelloComponent(){
-    return (
-        <section>
-            <article><h1>สวัสดี component เเบบ external</h1></article>
-        </section>
-    )
-  }
-
-  export default HelloComponent
